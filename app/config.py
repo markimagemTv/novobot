65a89484aa71c79a83ef8dd6c687aa068be7fce1
markimagemTv <markimagem2014@gmail.com>
2025-04-29 16:16:06 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
-MP_ACCESS_TOKEN = os.getenv("MP_ACCESS_TOKEN")
+MERCADO_PAGO_ACCESS_TOKEN = os.getenv("MERCADO_PAGO_ACCESS_TOKEN")  # Corrigido aqui
 BASE_URL = os.getenv("BASE_URL")
