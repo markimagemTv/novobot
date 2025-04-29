926b92f09c7587eb4f489bea5f75d63c6ccd5fbf
markimagemTv <markimagem2014@gmail.com>
2025-04-29 14:25:30 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
+MP_ACCESS_TOKEN = os.getenv("MP_ACCESS_TOKEN")
+BASE_URL = os.getenv("BASE_URL")  # URL pública do Render ou Heroku
